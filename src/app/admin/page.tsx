0bcc05bf08b6bf1100d92aa205770299d6a5f359
Jecky <jecky@JeckydeMacBook-Air.local>
2026-06-06 09:35:27 +0800

    🐛 Fix admin page route

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+export default function AdminPage() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>BEJADY CMS</title>
+      </head>
+      <body>
+        <script src="https://unpkg.com/decap-cms@3/dist/decap-cms.js" />
+      </body>
+    </html>
+  );
+}
